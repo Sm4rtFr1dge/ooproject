80dc8c413cf4c8cb62be7cb56d517e3f59f0be0c
Sm4rtFr1dge <ma09735@st.habib.edu.pk>
2025-12-06 17:10:28 +0500

    Final

--- Game.h
+++ Game.h
@@ -8,6 +8,7 @@
 #include "ShockManager.cpp"
 #include "Platform.h"
 
+// Define Game States
 enum GameState {
     STATE_MENU,
     STATE_CONTROLS,
@@ -19,46 +20,46 @@ class Game {
 private:
     sf::RenderWindow window;
     
-    // --- SHADER STUFF ---
-    sf::RenderTexture renderTexture; 
-    sf::Shader scanlineShader;       
-    sf::Sprite finalSprite;          
-    
     Player* player1;
     Player* player2;
     CollisionManager collisionManager;
-    ShockManager shockManager;
-    
-    // Restored Standard Background variables
-    sf::Texture bgTexture;
-    sf::Sprite bgSprite;
-    
+   
+    ShockManager shockManager{"/dev/ttyACM0"};
+
     sf::Texture platformTexture;
     std::vector<Platform> platforms;
+
+    sf::Texture bgTexture;
+    sf::Sprite bgSprite;
 
     // --- UI / STATE VARIABLES ---
     GameState currentState;
     sf::Font font;
 
+    // Menu Elements
     sf::Text titleText;
     sf::Text startButton;
     sf::Text controlsButton;
     sf::Text exitButton;
 
+    // Controls Elements
     sf::Text controlsTitle;
     sf::Text controlsContent; 
     sf::Text backButton;
 
+    // Game Over Elements
     sf::Text gameOverTitle;
     sf::Text winnerText;
     sf::Text restartButton;
     sf::Text quitButton;
 
+    // Helpers
     void initUI();
     void processEvents();
     void update(sf::Time dt);
     void draw();
     
+    // Specific Drawers
     void drawMenu();
     void drawControls();
     void drawHUD(); 
